b5e1945f7c3a747228965dd1f5a4321c064fd633
Shalvah Adebayo <shalvah.adebayo@konga.com>
2017-06-21 11:01:27 +0100

    Add support for Eloquent models in jsend_success Update PHPDoc

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,14 @@
 <?php
 
 if (!function_exists("jsend_error")) {
+    /**
+     * @param $message Error message
+     * @param string $code Optional custom error code
+     * @param string | array $data Optional data
+     * @param int $status HTTP status code
+     * @param array $extraHeaders
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
     function jsend_error($message, $code = "", $data = "", $status = 500, $extraHeaders = [])
     {
         $response = [
@@ -16,6 +24,12 @@ if (!function_exists("jsend_error")) {
 }
 
 if (!function_exists("jsend_fail")) {
+    /**
+     * @param $data
+     * @param int $status HTTP status code
+     * @param array $extraHeaders
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
     function jsend_fail($data, $status = 400, $extraHeaders = [])
     {
         $response = json_encode([
@@ -28,8 +42,15 @@ if (!function_exists("jsend_fail")) {
 }
 
 if (!function_exists("jsend_success")) {
+    /**
+     * @param array | Illuminate\Database\Eloquent\Model $data
+     * @param int $status HTTP status code
+     * @param array $extraHeaders
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
     function jsend_success($data = [], $status = 200, $extraHeaders = [])
     {
+        $data = ($data instanceof Illuminate\Database\Eloquent\Model) ? $data->toArray() : $data;
         $response = json_encode([
             "status" => "success",
             "data" => $data
