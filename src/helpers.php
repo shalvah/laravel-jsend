fc884ccec17c0e29bfcf22769ba3c35999db7694
Shalvah A <diakon.ng@gmail.com>
2017-06-22 08:12:30 +0100

    Implement helpers

---
+++ src/helpers.php
@@ -0,0 +1,61 @@
+<?php
+
+if (!function_exists("jsend_error")) {
+    /**
+     * @param string $message Error message
+     * @param string $code Optional custom error code
+     * @param string | array $data Optional data
+     * @param int $status HTTP status code
+     * @param array $extraHeaders
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    function jsend_error($message, $code = "", $data = "", $status = 500, $extraHeaders = [])
+    {
+        $response = [
+            "status" => "error",
+            "message" => $message
+        ];
+        if ($code) $response['code'] = $code;
+        if ($data) $response['data'] = $data;
+        $response = json_encode($response);
+        $headers = array_merge(["Content-type" => "application/json"], $extraHeaders);
+        return response($response, $status, $headers);
+    }
+}
+
+if (!function_exists("jsend_fail")) {
+    /**
+     * @param array $data
+     * @param int $status HTTP status code
+     * @param array $extraHeaders
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    function jsend_fail($data, $status = 400, $extraHeaders = [])
+    {
+        $response = json_encode([
+            "status" => "fail",
+            "data" => $data
+        ]);
+        $headers = array_merge(["Content-type" => "application/json"], $extraHeaders);
+        return response($response, $status, $headers);
+    }
+}
+
+if (!function_exists("jsend_success")) {
+    /**
+     * @param array | Illuminate\Database\Eloquent\Model $data
+     * @param int $status HTTP status code
+     * @param array $extraHeaders
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    function jsend_success($data = [], $status = 200, $extraHeaders = [])
+    {
+        $data = ($data instanceof Illuminate\Database\Eloquent\Model) ? $data->toArray() : $data;
+        $response = json_encode([
+            "status" => "success",
+            "data" => $data
+        ]);
+        $headers = array_merge(["Content-type" => "application/json"], $extraHeaders);
+        return response($response, $status, $headers);
+    }
+}
