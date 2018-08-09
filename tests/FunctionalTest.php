4b8b425197485df37be1a56172c50e8de26a2e23
shalvah <diakon.ng@gmail.com>
2018-08-09 03:30:31 +0100

    [WIP] Add tests

---
+++ tests/FunctionalTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by shalvah
+ * Date: 09-Aug-18
+ * Time: 03:26
+ */
+
+namespace Shalvah\LaravelJsend\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /** @test */
+    public function jsend_success_generates_a_response()
+    {
+        $this->assertTrue(true);
+    }
+}
