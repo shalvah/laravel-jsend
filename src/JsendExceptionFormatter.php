9b9157404ed205d0a88af0b02fb4dd05902afc28
Philip Newcomer <contact@philipnewcomer.net>
2020-01-22 13:41:27 -0500

    Add additional info to response if in debug mode

--- src/JsendExceptionFormatter.php
+++ src/JsendExceptionFormatter.php
@@ -3,6 +3,7 @@
 namespace Shalvah\LaravelJsend;
 
 use Exception;
+use Illuminate\Support\Arr;
 use Illuminate\Validation\ValidationException;
 
 /**
@@ -34,10 +35,24 @@ trait JsendExceptionFormatter
      */
     protected function prepareJsonResponse($request, Exception $e)
     {
+        $message = 'Server Error';
+        if (config('app.debug') || $this->isHttpException($e)) {
+            $message = $e->getMessage();
+        }
+
+        $data = config('app.debug') ? [
+            'exception' => get_class($e),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'trace' => collect($e->getTrace())->map(function ($trace) {
+                return Arr::except($trace, ['args']);
+            })->all(),
+        ] : null;
+
         return jsend_error(
-            $e->getMessage(),
+            $message,
             $e->getCode(),
-            null,
+            $data,
             $this->isHttpException($e) ? $e->getStatusCode() : 500,
             $this->isHttpException($e) ? $e->getHeaders() : []
         );
